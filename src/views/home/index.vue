217b9728820661bfe15047074a29f06f73931a77
GuoJiKun <guojikun486546@163.com>
2024-07-05 16:44:00 +0800

    feat: 增加选择插件目录组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import InstallPlugin from "@/components/install-plugin.vue";
 
+const handlerInstallSuccess = (path: string) => {
+  console.log("path", path);
+};
 </script>
 <template>
-    <div class="home"> Home Index
-        <router-link to="/search">Search</router-link>
+  <div class="home">
+    <h1>Home Index</h1>
+    <router-link to="/search">Search</router-link>
+    <div>
+      <InstallPlugin tag="button" @confirm="handlerInstallSuccess">
+        选择 目录
+      </InstallPlugin>
     </div>
+  </div>
 </template>
