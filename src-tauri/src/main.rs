99d3021411ef884deb676ce8d52bef8945f84e41
GuoJiKun <guojikun486546@163.com>
2024-07-10 10:58:56 +0800

    feat(rs): 添加执行二进制插件的逻辑

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn run_external_program(executable_path: String, args: Vec<String>) -> Result<String, String> {
+    let output = Command::new(executable_path)
+        .args(&args)
+        .output()
+        .map_err(|e| e.to_string())?;
+
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).to_string())
+    }
 }
 
 fn main() {
@@ -13,7 +24,7 @@ fn main() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![run_external_program])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
