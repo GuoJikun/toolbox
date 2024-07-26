dd5bf6f7b82ef2c644b18f7b0cb23f881ad5647d
GuoJiKun <guojikun486546@163.com>
2024-07-26 16:45:07 +0800

    build(rs): 使用installed_pkg替换installed

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use installed_pkg::Installed;
 use tauri::{command, webview};
 
 use libloading::{Library, Symbol};
@@ -86,36 +87,36 @@ pub struct InstalledApp {
     publisher: String,
 }
 
-#[command]
-fn get_installed_list() -> Vec<InstalledApp> {
-    let mut result: Vec<InstalledApp> = Vec::new();
-    let apps = installed::list();
-    match apps {
-        Err(_e) => {
-            return result;
-        }
-        Ok(res) => {
-            for app in res {
-                result.push(InstalledApp {
-                    name: app.name().to_string(),
-                    version: app.version().to_string(),
-                    publisher: app.publisher().to_string(),
-                });
-            }
-            return result;
-        }
-    }
-}
 // #[command]
-// fn get_installed_list() -> Vec<installed_pkg::App> {
-//     let apps = installed_pkg::list();
+// fn get_installed_list() -> Vec<InstalledApp> {
+//     let mut result: Vec<InstalledApp> = Vec::new();
+//     let apps = installed::list();
 //     match apps {
 //         Err(_e) => {
-//             return Vec::new();
+//             return result;
 //         }
-//         Ok(res) => res,
+//         Ok(res) => {
+//             for app in res {
+//                 result.push(InstalledApp {
+//                     name: app.name().to_string(),
+//                     version: app.version().to_string(),
+//                     publisher: app.publisher().to_string(),
+//                 });
+//             }
+//             return result;
+//         }
 //     }
 // }
+#[command]
+fn get_installed_list() -> Vec<installed_pkg::platform::App> {
+    let apps = installed_pkg::list();
+    match apps {
+        Err(_e) => {
+            return Vec::new();
+        }
+        Ok(res) => return res.apps,
+    }
+}
 
 fn get_lib_ext() -> String {
     if cfg!(target_os = "windows") {
