6fc0117a4f548b890d79e82be23e0b0a9a3c37f7
GuoJiKun <guojikun486546@163.com>
2024-08-01 17:05:46 +0800

    refactor(rs): 重构代码，相关的代码移动到单独的文件

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::command;
-
-use libloading::{Library, Symbol};
-use std::collections::HashMap;
-use std::ffi::{CStr, CString};
-
-use std::path::PathBuf;
 use std::process::Command;
+use tauri::command;
 use tauri_plugin_cli::CliExt;
 
-pub mod plugins;
+// 插件相关
+mod plugins;
 use plugins::{run_node_script, run_php_script, run_python_script};
+// 动态库相关/ffi
+mod dylib;
+use dylib::dynamic_command;
 
 #[command]
 fn run_external_program(executable_path: String, args: Vec<String>) -> Result<String, String> {
@@ -25,24 +23,6 @@ fn run_external_program(executable_path: String, args: Vec<String>) -> Result<St
         Ok(String::from_utf8_lossy(&output.stdout).to_string())
     } else {
         Err(String::from_utf8_lossy(&output.stderr).to_string())
-    }
-}
-
-#[command]
-fn dynamic_command(plugin: String, fn_name: String) -> Result<String, String> {
-    let lib_path_map = collect_dylib();
-
-    let lib_path = lib_path_map.get(&plugin).unwrap();
-    // 动态生成函数名并转换为以空终止符结尾的 CString
-    let func_name_cstr = CString::new(fn_name).map_err(|e| e.to_string())?;
-    unsafe {
-        let lib = Library::new(lib_path).map_err(|e| e.to_string())?;
-        let func: Symbol<unsafe extern "C" fn() -> *const i8> = lib
-            .get(func_name_cstr.as_bytes_with_nul())
-            .map_err(|e| e.to_string())?;
-        let result_c_str = CStr::from_ptr(func());
-        let result_str = result_c_str.to_str().map_err(|e| e.to_string())?;
-        Ok(result_str.to_string())
     }
 }
 
@@ -75,49 +55,6 @@ fn get_installed_list() -> Vec<installed_pkg::platform::App> {
         }
         Ok(res) => return res.apps,
     }
-}
-
-fn get_lib_ext() -> String {
-    if cfg!(target_os = "windows") {
-        return String::from(".dll");
-    } else if cfg!(target_os = "macos") {
-        return String::from(".dylib");
-    } else {
-        return String::from(".so");
-    }
-}
-fn collect_dylib() -> HashMap<String, PathBuf> {
-    let ext = get_lib_ext();
-    let mut handlers: HashMap<String, PathBuf> = HashMap::new();
-    let plugins_dir = std::env::current_exe()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .join("plugins");
-
-    plugins_dir
-        .read_dir()
-        .unwrap()
-        .for_each(|entry| match entry {
-            Ok(entry) => {
-                let path = entry.path();
-
-                if path.is_dir() {
-                    // let tmp_path = plugins_dir.clone();
-                    let lib_path = path.join("lib").join(format!("index{}", ext));
-                    if lib_path.exists() {
-                        handlers.insert(
-                            path.file_name().unwrap().to_str().unwrap().to_string(),
-                            lib_path.clone(),
-                        );
-                    }
-                }
-            }
-            Err(e) => {
-                eprintln!("Failed to read directory: {}", e);
-            }
-        });
-    return handlers;
 }
 
 fn main() {
