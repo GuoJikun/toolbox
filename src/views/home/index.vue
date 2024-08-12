ed237e9d40f7bde399b43f3b6f8f22768e9a8f30
GuoJiKun <guojikun486546@163.com>
2024-08-12 17:20:42 +0800

    feat: 增加截图Command的示例

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,49 +1,30 @@
 <script setup lang="ts">
 import InstallPlugin from '@/components/install-plugin.vue'
 import { invoke } from '@tauri-apps/api/core'
-import { getCurrentWindow } from '@tauri-apps/api/window'
+import { getCurrentWindow } from '@/utils/index'
 
 const handlerInstallSuccess = (path: string) => {
     console.log('path', path)
 }
 
-const handleBeforePermission = () => {
-    console.log('添加权限前')
-    const currentWindow = getCurrentWindow()
-    currentWindow.isMaximized().then((res) => {
-        console.log('isMaximized', res)
-    })
-}
-const handlePermission = () => {
-    console.log('添加权限')
-    invoke('add_capabilities', {
-        window: 'main',
-        webview: '',
-        permissions: ['window:allow-is-maximize']
-    }).then((res) => {
-        console.log('add_capabilities', res)
-    })
-}
-
-const handleAfterPermission = () => {
-    console.log('添加权限后')
-    const currentWindow = getCurrentWindow()
-    currentWindow.isMaximized().then((res) => {
-        console.log('isMaximized', res)
-    })
+const getScreenshot = async () => {
+    const path = await invoke('screenshot_desktop')
+    console.log('res', path)
+    if (!path) {
+        console.log('截图失败')
+        return
+    }
 }
 </script>
 <template>
     <div class="home">
         <h1>Home Index</h1>
         <el-space>
-            <el-button @click="handleBeforePermission">添加权限前</el-button>
-            <el-button @click="handlePermission">添加权限</el-button>
-            <el-button @click="handleAfterPermission">添加权限后</el-button>
+            <el-button @click="getScreenshot">获取屏幕截图</el-button>
         </el-space>
 
         <div>
-            <InstallPlugin tag="button" @confirm="handlerInstallSuccess"> 选择 目录 </InstallPlugin>
+            <InstallPlugin tag="button" @confirm="handlerInstallSuccess"> 选择目录 </InstallPlugin>
         </div>
     </div>
 </template>
