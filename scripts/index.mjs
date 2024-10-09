1b3e9bac29893f34b30a6e8a9ec4a78cd0d4facc
GuoJiKun <guojikun486546@163.com>
2024-10-09 09:48:43 +0800

    feat: 更新处理插件相关的脚本

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -10,24 +10,20 @@ const buildPlugins = async () => {
     const root = getRootDir()
     const pluginRoot = join(root, 'plugins')
     const destRoot = join(root, 'src-tauri', 'resources', 'plugins')
-    if (!fs.exists(destRoot)) {
-        await fs.mkdir(destRoot)
-    }
+    await fs.emptyDir(destRoot)
     const pluginDirs = await fs.readdir(join(pluginRoot, 'workspaces'))
 
-    console.log('pluginDirs', pluginDirs)
     for (const plugin of pluginDirs) {
         const src = join(pluginRoot, 'workspaces', plugin, 'dist')
         const dest = join(destRoot, plugin)
         await fs.copy(src, dest)
     }
-    // })
 }
 
 const copyBinaries = async () => {
     const root = getRootDir()
     const src = join(root, 'binaries')
-    const dest = join(root, 'src-tauri', 'src', 'binaries')
+    const dest = join(root, 'src-tauri', 'binaries')
     if (!fs.exists(dest)) {
         await fs.mkdir(dest)
     }
@@ -39,7 +35,6 @@ const copyBinaries = async () => {
 }
 
 const run = async () => {
-    console.log('Hello World!')
     await buildPlugins()
     await copyBinaries()
 }
