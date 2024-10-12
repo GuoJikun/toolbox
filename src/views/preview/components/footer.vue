ed2ff9e6c30480f8de04672660f0a3ac6e467202
GuoJiKun <guojikun486546@163.com>
2024-10-12 17:01:24 +0800

    feat: 增加视频和图片格式的预览

---
+++ src/views/preview/components/footer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="footer">footer</div>
+</template>
+
+<style scoped lang="scss">
+.footer{
+    height: 20px;
+    padding: 0 8px;
+    background: #f5f5f5;
+    display: flex;
+    align-items: center;
+    align-content: center;
+}
+</style>
